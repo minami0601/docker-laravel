42c0b062a377bf8bfd9ef2c794531ab6810105ee
minami0601 <minami.love.bb@icloud.com>
2023-01-09 15:23:19 +0900

    お問い合わせ一覧作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('contacts')
+    ->middleware(['auth'])
+    ->name('contacts.')
+    ->controller(ContactFormController::class)
+    ->group(function(){
+        Route::get('/', 'index')->name('index');
+    });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
