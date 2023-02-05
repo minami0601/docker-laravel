825c0dbcda38151afdc05da250c6ad99861abd7f
minami0601 <minami.love.bb@icloud.com>
2023-02-05 15:58:23 +0900

    TweetのDTOを追加

---
+++ src/packages/Domain/Tweet/Dto/TweetInputData.php
@@ -0,0 +1,32 @@
+<?
+namespace App\Packages\Domain\Tweet\Dto;
+
+class TweetInputData
+{
+private $user_id;
+private $content;
+
+public function __construct($user_id, $content)
+{
+$this->user_id = $user_id;
+$this->content = $content;
+}
+
+/**
+* @return mixed
+*/
+public function getUserId()
+{
+return $this->user_id;
+}
+
+/**
+* @return mixed
+*/
+public function getContent()
+{
+return $this->content;
+}
+}
+
+?>
