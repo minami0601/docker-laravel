b3e4935439bbbf930338e5136344203dbba2c83b
minami0601 <minami.love.bb@icloud.com>
2023-02-05 15:58:50 +0900

    インデント修正

--- src/packages/Domain/Tweet/Dto/TweetInputData.php
+++ src/packages/Domain/Tweet/Dto/TweetInputData.php
@@ -3,30 +3,30 @@ namespace App\Packages\Domain\Tweet\Dto;
 
 class TweetInputData
 {
-private $user_id;
-private $content;
+  private $user_id;
+  private $content;
 
-public function __construct($user_id, $content)
-{
-$this->user_id = $user_id;
-$this->content = $content;
-}
+  public function __construct($user_id, $content)
+  {
+    $this->user_id = $user_id;
+    $this->content = $content;
+  }
 
-/**
-* @return mixed
-*/
-public function getUserId()
-{
-return $this->user_id;
-}
+  /**
+  * @return mixed
+  */
+  public function getUserId()
+  {
+    return $this->user_id;
+  }
 
-/**
-* @return mixed
-*/
-public function getContent()
-{
-return $this->content;
-}
+  /**
+  * @return mixed
+  */
+  public function getContent()
+  {
+    return $this->content;
+  }
 }
 
 ?>
